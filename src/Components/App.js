a0dd750a2d2dc2c15db725eab8e31cdf2e0f1cd0
Anubhab Patnaik <anubhabr50@gmail.com>
2022-01-31 17:44:45 +0530

    final commit

--- src/Components/App.js
+++ src/Components/App.js
@@ -4,15 +4,21 @@ import Login from './Login'
 import Dashboard from './Dashboard'
 import { AuthProvider } from '../contexts/AuthContext'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import PrivateRoute from './PrivateRoute'
+import ForgotPassword from './ForgotPassword'
+import UpdateProfile from './UpdateProfile'
 
 function App () {
   return (
     <Router>
       <AuthProvider>
         <Routes>
+          <Route exact path='/' element={<Dashboard />} />
+          <Route path='/update-profile' element={<UpdateProfile />} />
           <Route path='/signup' caseSensitive={false} element={<Signup />} />
           <Route exact path='/' caseSensitive={false} element={<Dashboard />} />
           <Route path='/login' caseSensitive={false} element={<Login />} />
+          <Route path='/forgot-password' component={ForgotPassword} />
         </Routes>
       </AuthProvider>
     </Router>
