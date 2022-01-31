aaa0f62aa0faafe4234b56d39b9fac7d2268a5d7
Anubhab Patnaik <anubhabr50@gmail.com>
2022-01-31 17:44:39 +0530

    final commit

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -14,8 +14,25 @@ export function AuthProvider ({ children }) {
   function signup (email, password) {
     return auth.createUserWithEmailAndPassword(email, password)
   }
+
   function login (email, password) {
     return auth.signInWithEmailAndPassword(email, password)
+  }
+
+  function logout () {
+    return auth.signOut()
+  }
+
+  function resetPassword (email) {
+    return auth.sendPasswordResetEmail(email)
+  }
+
+  function updateEmail (email) {
+    return currentUser.updateEmail(email)
+  }
+
+  function updatePassword (password) {
+    return currentUser.updatePassword(password)
   }
 
   useEffect(() => {
@@ -29,7 +46,11 @@ export function AuthProvider ({ children }) {
   const value = {
     currentUser,
     login,
-    signup
+    signup,
+    logout,
+    resetPassword,
+    updateEmail,
+    updatePassword
   }
   return (
     <AuthContext.Provider value={value}>
